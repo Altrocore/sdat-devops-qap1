da45740af80d3fb75212cc8c0deb92a4ff38b0df
Altrocore <shepelev.daniel.ca@gmail.com>
2024-01-29 22:31:11 -0330

    first init maven and changed first version of SuggestionEngine (null check), also SuggestionDatabase (lombok integration)

---
+++ src/main/java/com/clover/Invoice.java
@@ -0,0 +1,4 @@
+package com.clover;
+
+public class Invoice {
+}
---
+++ src/main/java/com/clover/SuggestionsDatabase.java
@@ -0,0 +1,26 @@
+package com.clover;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class SuggestionsDatabase {
+    private Map<String,Integer> wordMap = new HashMap<>();
+
+    /*
+    public Map<String, Integer> getWordMap() {
+        if (wordMap == null) {
+            wordMap = new HashMap<String, Integer>();
+        }
+
+        return wordMap;
+    }
+
+    public void setWordMap(Map<String, Integer> wordMap) {
+        this.wordMap = wordMap;
+    }*/
+}
